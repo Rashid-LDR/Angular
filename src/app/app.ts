a0120bbd0a230eefc6e983e29990e61ae06946b8
Rashid Ali <mrashidali51@gmail.com>
2026-03-04 22:14:18 +0500

    COunter component added with increase & decrease reset values/ Todoses component with routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComopnent } from "./Component/home-comopnent/home-comopnent";
-import { Greeding } from './Component/greeding/greeding';
 import { HeaderComponent } from './Component/header-component/header-component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,HomeComopnent],
-  template: `<app-header-component/> <app-home-comopnent/> `,
+  imports: [HeaderComponent, RouterOutlet],
+  template: `<app-header-component/> <router-outlet/> `,
   styleUrl: './app.css'
 })
 export class App {
