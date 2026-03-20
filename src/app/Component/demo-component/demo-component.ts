83a357f03ea3b07600e91783e10414e6ba15aded
Rashid Ali <mrashidali51@gmail.com>
2026-03-21 00:18:46 +0500

    Demo serverices and component is added

---
+++ src/app/Component/demo-component/demo-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-component',
+  imports: [],
+  templateUrl: './demo-component.html',
+  styleUrl: './demo-component.css',
+})
+export class DemoComponent {
+
+}
